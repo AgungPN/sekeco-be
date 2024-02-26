72f8dd5bedbea4608a0075a8620e35e44d10dba1
AgungPN <agungpn33@gmail.com>
2024-02-26 09:02:44 +0700

    first commit

---
+++ skeco-be/src/main/java/com/cashier/system/skecobe/SkecoBeApplication.java
@@ -0,0 +1,13 @@
+package com.cashier.system.skecobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkecoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkecoBeApplication.class, args);
+	}
+
+}
---
+++ skeco-be/src/test/java/com/cashier/system/skecobe/SkecoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashier.system.skecobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkecoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
