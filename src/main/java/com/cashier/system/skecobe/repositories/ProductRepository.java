b1a08889cb82d71ea063c4b4a98201e3ce0f66d4
AgungPN <agungpn33@gmail.com>
2024-03-07 08:39:29 +0700

    feat: add feature to validate unique value of new barcode on purchases

--- src/main/java/com/cashier/system/skecobe/repositories/ProductRepository.java
+++ src/main/java/com/cashier/system/skecobe/repositories/ProductRepository.java
@@ -2,10 +2,11 @@ package com.cashier.system.skecobe.repositories;
 
 import com.cashier.system.skecobe.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
     Optional<Product> findByBarcode(String barcode);
+    List<Product> findByBarcodeIn(List<String> barcodes);
 }
